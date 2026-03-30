d1580f14eb8e5d65aa3365523a8c491217198a51
sherry <shaheryarm58@gmail.com>
2026-03-30 12:24:11 +0000

    Added MongoDB with volume persistence

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
-app.get('/api/status', (req, res) => {
-    res.json({
-        status: "running",
+// MongoDB connection
+mongoose.connect('mongodb://mongo:27017/devopsdb')
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// Simple schema
+const StatusSchema = new mongoose.Schema({
+    message: String,
+    timestamp: Date
+});
+
+const Status = mongoose.model('Status', StatusSchema);
+
+// API route
+app.get('/api/status', async (req, res) => {
+    const data = new Status({
         message: "Backend is running 🚀",
         timestamp: new Date()
+    });
+
+    await data.save();
+
+    const allData = await Status.find().sort({ timestamp: -1 }).limit(5);
+
+    res.json({
+        status: "running",
+        history: allData
     });
 });
 
